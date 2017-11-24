3753b11927a5597036441a5dda75f22c9641f50a
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2017-11-24 01:01:44 +0000

    no longer need to input the size of the matrix

--- Python/MatrixVectorMult.py
+++ Python/MatrixVectorMult.py
@@ -1,8 +1,8 @@
 A = [[1,2,3],[4,5,6],[7,8,9]]
 B = [1,2,3]
-n = 3
 
-def MatrixVectorMult(A, B, n):
+def MatrixVectorMult(A, B):
+    n = len(A)
     C = [None] * n
     for i in range(n):
         C[i] = 0
@@ -10,4 +10,4 @@ def MatrixVectorMult(A, B, n):
             C[i] = C[i] + A[i][j] * B[j]
     return C
 
-print MatrixVectorMult(A, B, n)
+print MatrixVectorMult(A, B)
--- Python/ScalarProduct.py
+++ Python/ScalarProduct.py
@@ -1,11 +1,11 @@
 A = [1,2,3]
 B = [4,5,6]
-n = 3
 
-def ScalarProduct(A, B, n):
+def ScalarProduct(A, B):
+    n = len(A)
     S = 0
     for i in range(n):
-        S = S + A[i]*B[i]
+        S = S + A[i] * B[i]
     return S
 
-print ScalarProduct(A, B, n)
+print ScalarProduct(A, B)
